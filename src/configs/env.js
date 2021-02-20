dab871fcfc0ba097a8c4aaef5a357377d5d6ada2
namhihi237 <yentth237@gmail.com>
2021-02-20 19:51:28 +0700

    url db

--- src/configs/env.js
+++ src/configs/env.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 export const envVariables = {
     port: process.env.PORT || 5000,
-    mongoURI: process.env.DB_URI || "mongodb://localhost:27017/cnpm",
+    mongoURI:
+        process.env.DB_URI ||
+        "mongodb+srv://cnpm:cnpm17t1@cluster0.n1nom.mongodb.net/todoapp?retryWrites=true&w=majority",
     jwtSecret: process.env.JWT_SECRET || "doancnpm!@#",
     nodeEnv: process.env.NODE_ENV || "development",
     gmail: process.env.GMAIL,
