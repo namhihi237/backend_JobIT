e4a6e0ebe5763768624e942b7434837558af4a8b
namhihi <yentth237@gmail.com>
2021-04-25 12:20:29 +0700

    fix index post

--- src/models/Post.model.js
+++ src/models/Post.model.js
@@ -1,69 +1,69 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 const PostSchema = new Schema(
-    {
-        companyId: {
-            type: Schema.Types.ObjectId,
-            ref: "account",
-            require: true,
-        },
-        companyName: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-        },
-        skill: [
-            {
-                type: String,
-            },
-        ],
-        position: [
-            {
-                type: String,
-            },
-        ],
+	{
+		companyId: {
+			type: Schema.Types.ObjectId,
+			ref: 'account',
+			require: true,
+		},
+		companyName: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+		},
+		skill: [
+			{
+				type: String,
+			},
+		],
+		position: [
+			{
+				type: String,
+			},
+		],
 
-        address: {
-            type: String,
-        },
+		address: {
+			type: String,
+		},
 
-        salary: {
-            type: String,
-        },
-        apply: [
-            {
-                iterId: {
-                    type: Schema.Types.ObjectId,
-                    ref: "account",
-                },
-                cvId: {
-                    type: Schema.Types.ObjectId,
-                    ref: "cv",
-                },
-                fullName: { type: String },
-                email: { type: String },
-            },
-        ],
-        endTime: {
-            type: String,
-        },
-        description: {
-            type: String,
-        },
-        comment: [{ type: String, default: [] }],
-        accept: {
-            type: Boolean,
-            default: false,
-        },
-        active: {
-            type: Boolean,
-            default: true,
-        },
-    },
-    { timestamps: true },
+		salary: {
+			type: String,
+		},
+		apply: [
+			{
+				iterId: {
+					type: Schema.Types.ObjectId,
+					ref: 'account',
+				},
+				cvId: {
+					type: Schema.Types.ObjectId,
+					ref: 'cv',
+				},
+				fullName: { type: String },
+				email: { type: String },
+			},
+		],
+		endTime: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		comment: [{ type: String, default: [] }],
+		accept: {
+			type: Boolean,
+			default: false,
+		},
+		active: {
+			type: Boolean,
+			default: true,
+		},
+	},
+	{ timestamps: true },
 );
+PostSchema.indexs({ companyName: 'text', description: 'text', position: 'text', skill: 'text', address: 'text' });
 
-export const Post = model("post", PostSchema, "post");
-Post.createIndexes({ companyName: "text", description: "text", position: "text", skill: "text" });
+export const Post = model('post', PostSchema, 'post');
