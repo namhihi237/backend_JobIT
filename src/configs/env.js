a24b16d839f629b779d85421a96af05d8541469f
namhihi <thap.nguyen+code@wequilt.com>
2021-05-08 15:45:04 +0700

    fix email

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
+require('dotenv').config();
 
 export const envVariables = {
-    port: process.env.PORT || 5000,
-    mongoURI:
-        process.env.DB_URI ||
-        "mongodb+srv://cnpm:cnpm17t1@cluster0.n1nom.mongodb.net/jobIT?retryWrites=true&w=majority",
-    jwtSecret: process.env.JWT_SECRET || "doancnpm!@#",
-    nodeEnv: process.env.NODE_ENV || "development",
-    gmail: process.env.GMAIL || "poppy99.dev@gmail.com",
-    pass: process.env.PASSWORD || "letrungnam23799@",
-    subject: process.env.SUBJECT || "[CODE RESET :]",
-    cloud_name: process.env.CLOUD_NAME || "do-an-cnpm",
-    api_key: process.env.API_KEY_CLOUD || "484176915684615",
-    api_secret: process.env.API_SECRET_CLOUD || "hpWWOxyc-cm_Egs5bqRF4UzPJf8",
-    key_admin: "123qwe!@#",
-    url_fe: "http://localhost/",
+	port: process.env.PORT || 5000,
+	mongoURI:
+		process.env.DB_URI ||
+		'mongodb+srv://cnpm:cnpm17t1@cluster0.n1nom.mongodb.net/jobIT?retryWrites=true&w=majority',
+	jwtSecret: process.env.JWT_SECRET || 'doancnpm!@#',
+	nodeEnv: process.env.NODE_ENV || 'development',
+	gmail: process.env.GMAIL || 'poppy99.dev@gmail.com',
+	pass: process.env.PASSWORD || 'Namvippro23799@',
+	subject: process.env.SUBJECT || '[CODE RESET :]',
+	cloud_name: process.env.CLOUD_NAME || 'do-an-cnpm',
+	api_key: process.env.API_KEY_CLOUD || '484176915684615',
+	api_secret: process.env.API_SECRET_CLOUD || 'hpWWOxyc-cm_Egs5bqRF4UzPJf8',
+	key_admin: '123qwe!@#',
+	url_fe: 'http://localhost/',
 };
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,50 +1,58 @@
-import nodemailer from "nodemailer";
-import { envVariables } from "../configs";
+import nodemailer from 'nodemailer';
+import { envVariables } from '../configs';
 const { gmail, pass, subject } = envVariables;
-const ALPHABET = "0123456789ABCDEFGHIKLMNOPQRSTUVWXYZ";
-import { passwordResetTemplate } from "../resources";
+const ALPHABET = '0123456789ABCDEFGHIKLMNOPQRSTUVWXYZ';
+import { passwordResetTemplate } from '../resources';
 
 export const sendMailJob = async (email, skill, linkJob) => {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: gmail,
-            pass: pass,
-        },
-    });
-    await transporter.sendMail({
-        from: gmail,
-        to: email,
-        subject: `[NEW JOB FOR YOU]`,
-        text: `Đã có job mới về ${skill} tại ${linkJob}`,
-    });
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: gmail,
+			pass: pass,
+		},
+	});
+	try {
+		await transporter.sendMail({
+			from: gmail,
+			to: email,
+			subject: `[NEW JOB FOR YOU]`,
+			text: `Đã có job mới về ${skill} tại ${linkJob}`,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const sendEmail = async (code, email) => {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: gmail,
-            pass: pass,
-        },
-    });
-    await transporter.sendMail({
-        from: gmail,
-        to: email,
-        subject: subject,
-        html: passwordResetTemplate(code),
-    });
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: gmail,
+			pass: pass,
+		},
+	});
+	try {
+		await transporter.sendMail({
+			from: gmail,
+			to: email,
+			subject: subject,
+			html: passwordResetTemplate(code),
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // generate code 6 char
 export const generate = () => {
-    let code = "";
-    for (let i = 0; i < 6; i++) {
-        code += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
-    }
-    return code;
+	let code = '';
+	for (let i = 0; i < 6; i++) {
+		code += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+	}
+	return code;
 };
