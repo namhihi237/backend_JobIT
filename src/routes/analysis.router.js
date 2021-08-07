afe88e954848c0a53288d7747b77287a6ed07244
namhihi <yentth237@gmail.com>
2021-08-07 10:34:10 +0700

    fix permission

--- src/routes/analysis.router.js
+++ src/routes/analysis.router.js
@@ -10,6 +10,6 @@ export const analysisRouter = Router();
 
 analysisRouter.route('/api/v1/analysis/post').get(authMiddleware.jwtMidleware,checkPer(ACTION_CODE.ANALYSIS), analysisController.analysisOfPost);
 
-analysisRouter.route('/api/v1/analysis/skill').get(authMiddleware.jwtMidleware, analysisController.analysisOfSkill);
+analysisRouter.route('/api/v1/analysis/skill').get(authMiddleware.jwtMidleware,checkPer(ACTION_CODE.ANALYSIS), analysisController.analysisOfSkill);
 
 analysisRouter.route('/api/v1/analysis/user').get(authMiddleware.jwtMidleware,checkPer(ACTION_CODE.ANALYSIS), analysisController.analysisUser);
