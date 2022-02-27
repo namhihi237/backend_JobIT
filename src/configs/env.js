39f66846962df7d0d192e99d09818f3729025fff
namhihi237 <yentth237@gmail.com>
2022-02-27 18:43:21 +0700

    fixs some env

--- src/configs/env.js
+++ src/configs/env.js
@@ -4,18 +4,18 @@ export const envVariables = {
 	port: process.env.PORT || 5000,
 	mongoURI:
 		process.env.DB_URI ||
-		'mongodb+srv://cnpm:cnpm17t1@cluster0.n1nom.mongodb.net/dbTest?retryWrites=true&w=majority',
+		'',
 	jwtSecret: process.env.JWT_SECRET || 'doancnpm!@#',
 	nodeEnv: process.env.NODE_ENV || 'development',
 	subject: process.env.SUBJECT || 'CODE RESET PASSWORD',
 	cloud_name: process.env.CLOUD_NAME || 'do-an-cnpm',
-	api_key: process.env.API_KEY_CLOUD || '484176915684615',
-	api_secret: process.env.API_SECRET_CLOUD || 'hpWWOxyc-cm_Egs5bqRF4UzPJf8',
+	api_key: process.env.API_KEY_CLOUD || '',
+	api_secret: process.env.API_SECRET_CLOUD || '',
 	key_admin: '123qwe!@#',
 	url_fe: 'https://it-jobs-app.netlify.app/#',
 	SENDGRID_API_KEY:
-		process.env.SENDGRID_API_KEY || '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',
+		process.env.SENDGRID_API_KEY || '',
 	VERIFIED_SENDER: process.env.VERIFIED_SENDER || 'poppy99.dev@gmail.com',
 	gmail: 'poppy99.dev@gmail.com',
-	pass: 'namvippro23799@',
+	pass: process.env.PASSWORD || '',
 };
